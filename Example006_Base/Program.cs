e4817d13cc8e5f21482e158090c591ab1fa556f4
Denis <damned_wolf@mail.ru>
2023-01-24 23:42:59 +0300

    Example 006 Base

---
+++ Example006_Base/Program.cs
@@ -0,0 +1,15 @@
+﻿int a = 1, b = 8, c = 3, d = 2, e = 6;
+int max = a;
+if (b > max){
+    max = b;
+}
+else if (c > max) {
+    max = c;
+}
+else if (d > max){
+    max = d;
+}
+else if (e > max){
+    max = e;
+}
+Console.WriteLine(max);
