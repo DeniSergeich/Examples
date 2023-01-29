5a9260a528e80755d4bb3865af8bb05cd0e76ed4
Denis <damned_wolf@mail.ru>
2023-01-29 21:09:42 +0300

    Example 010 Method Array

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = { 111, 92, 66, 41, 58, 66, 57, 58 };
+
+int n = array.Length;
+int find = 66;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
