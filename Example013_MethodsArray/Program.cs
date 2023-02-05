af91877bb44274b23d662f7e4f06b50ff20e54f5
Denis <damned_wolf@mail.ru>
2023-02-05 18:29:11 +0300

    Example 013 Methods Array

---
+++ Example013_MethodsArray/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.Clear();
+int[] array = { 1, 5, 6, 7, 5, 1, 6, 2, 3, 8, 4, 10 };
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;
+
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] < array[minPosition])
+            {
+                minPosition = j;
+            }
+        }
+
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+    }
+}
+PrintArray(array);
+SelectionSort(array);
+PrintArray(array);
